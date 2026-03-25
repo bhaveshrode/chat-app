a3dd2a714e713f6c1fe70110fffe8e9bc6b21f47
bhaveshrode <bhaveshrode2004@gmail.com>
2026-03-25 17:52:02 +0530

    Add user registration and login API functions

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,3 +7,14 @@ export const api = axios.create({
 export const setAuthToken = (token) => {
   api.defaults.headers.common.Authorization = token ? `Bearer ${token}` : '';
 };
+
+// Auth APIs
+export const registerUser = async (data) => {
+    const res = await api.post('/auth/register', data);
+    return res.data;
+};
+
+export const loginUser = async (data) => {
+    const res = await api.post('/auth/login', data);
+    return res.data;
+};
