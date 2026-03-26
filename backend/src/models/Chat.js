ced5ddbd932e6158c05a58c763b0ec0dcbbc75f4
bhaveshrode <bhaveshrode2004@gmail.com>
2026-03-27 00:30:29 +0530

    Add lastMessage field to Chat schema

--- backend/src/models/Chat.js
+++ backend/src/models/Chat.js
@@ -1,13 +1,27 @@
 import mongoose from 'mongoose';
 
 const chatSchema = new mongoose.Schema(
-  {
-    name: String,
-    isGroup: { type: Boolean, default: false },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-  },
-  { timestamps: true }
+    {
+        name: String,
+        isGroup: {
+            type: Boolean,
+            default: false
+        },
+        members: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }],
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        lastMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message"
+        }
+    },
+    { timestamps: true }
 );
 
 export const Chat = mongoose.model('Chat', chatSchema);
